3067d388e9cbc31510319e7c5dae7f306627b23a
Daniel "soro" Cunha <danielsoro@gmail.com>
2020-05-01 08:58:44 -0300

    Removing Phrase struct. fixes #4

--- main.go
+++ main.go
@@ -10,25 +10,21 @@ import (
 	"github.com/gofiber/fiber"
 )
 
-type Phrases struct {
-	value string
-}
-
-func getPhrases() []Phrases {
-	return []Phrases{
-		{"Eu te amo!!"},
-		{"Não vejo a hora de dormir contigo em meus braços todas as noites"},
-		{"Já disse que tu é muito gostosa? Tu é MUITOO GOSTOSA!!"},
-		{"Não existe princesa mais linda do que você, nem na Disney"},
-		{"Minha tchutchuquinha, que eu amo muitcho!!!"},
-		{"Já disse que tu é muito linda? Tu é MUITOO LINDA!!"},
+func getPhrases() []string {
+	return []string{
+		"Eu te amo!!",
+		"Não vejo a hora de dormir contigo em meus braços todas as noites",
+		"Já disse que tu é muito gostosa? Tu é MUITOO GOSTOSA!!",
+		"Não existe princesa mais linda do que você, nem na Disney",
+		"Minha tchutchuquinha, que eu amo muitcho!!!",
+		"Já disse que tu é muito linda? Tu é MUITOO LINDA!!",
 	}
 }
 
 func handler(ctx *fiber.Ctx) {
 	var phrases = getPhrases()
 	ctx.Set(fiber.HeaderContentType, "text/html; charset=utf-8")
-	ctx.Send(fmt.Sprintf("<h1>%s</h1>", phrases[rand.Intn(len(phrases))].value))
+	ctx.Send(fmt.Sprintf("<h1>%s</h1>", phrases[rand.Intn(len(phrases))]))
 }
 
 func main() {
