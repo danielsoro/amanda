0893fcf797094be3b83882d435ca1e19566327ea
Daniel "soro" Cunha <danielsoro@gmail.com>
2020-05-06 18:31:05 -0300

    Creating template folder for project's template fixes #18

--- middlewares/notFoundMiddleware.go
+++ middlewares/notFoundMiddleware.go
@@ -2,9 +2,10 @@ package middlewares
 
 import "github.com/gofiber/fiber"
 
+// NotFoundHandler Render the default NotFound page
 func NotFoundHandler(c *fiber.Ctx) {
 	c.Status(404)
-	err := c.Render("view/notfound.html", nil)
+	err := c.Render("template/view/notfound.html", nil)
 	if err != nil {
 		c.Status(fiber.StatusInternalServerError)
 	}
